2178013894c4351d3701c00a5fb4a217e004a99f
Mark Ignacio <mark@ignacio.io>
2016-01-07 19:11:45 -0500

    10 and 11 points

---
+++ src/10points.php
@@ -0,0 +1,10 @@
+<?php
+// **************************************************************
+// First 10 points: make this script not crash and display "What's up, Docker?"
+// **************************************************************
+$whassup = "What's up,";
+$doc = "Docker?";
+
+echo $whassup + $doc;
+
+>
